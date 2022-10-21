0170d70d51f8a7941f4d40819e1c58716ebf264b
andyrtq <andycabrera259@gmail.com>
2022-10-21 17:13:46 -0500

    Creaccion nueva clase llamada virtualizacion

---
+++ src/virtualizacion.java
@@ -0,0 +1,8 @@
+public class virtualizacion {
+
+    public static void main(String[] args) {
+        System.out.println("TEMAS DE PROYECTOS EXPLICADO EN PROYECTO DE CONVENCIONES");
+        System.out.println("Unos de los temas que me intereso fue");
+        System.out.println();
+    }
+}
