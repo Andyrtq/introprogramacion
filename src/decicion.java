d4e3264ec84b80bca1db57205a4d3f4483d0209c
Andyrtq <116389523+Andyrtq@users.noreply.github.com>
2022-11-01 17:31:43 -0500

    Add files via upload

---
+++ src/decicion.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Programam para determinar si una persona es mayor o menor de edad
+ */
+public class decicion {
+    public static void main(String[] args) {
+        System.out.println("Programa para determinar si una persona es mayor de edad");
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese su edad");
+        int edad = teclado.nextInt();
+        if(edad >= 18 ) {
+            System.out.println("Eres mayor de edad");
+        }else{
+            System.out.println("Eres menor de edad");
+            System.out.println("Te falta madurar");
+        }
+
+    }
+}
